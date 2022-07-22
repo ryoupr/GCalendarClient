82270221e1bd26f3f72347039208e375e237c5c3
ryoupr <ryou120710@gmail.com>
2022-07-22 15:38:01 +0900

    非終日イベント連続追加の仕組み作成

--- addschedules.py
+++ addschedules.py
@@ -1,4 +1,5 @@
 from audioop import add
+from operator import le
 from stringprep import in_table_c9
 from tracemalloc import start
 from http.client import OK
@@ -53,10 +54,11 @@ def add_schedules(values):
         # カンマ区切りでリストを作成
         startdates = values['startDate'].split(',')
         enddates = values['endDate'].split(',')
-        if len(startdates) != len(enddates):
-            enddates = startdates
         print(f'statdates are {startdates}')
         print(f'enddates  are {enddates} ')
+        # 開始日・終了日の要素数が違ったら終了日を開始日と同一とする
+        if len(startdates) != len(enddates):
+            enddates = startdates
         # forで回しながら予定を追加
         for i in range(0, len(startdates)):
             values['startDate'] = startdates[i]
@@ -92,17 +94,68 @@ calendarEvent
                 'timeZone': 'Japan',
             },
         }
+        # 開始日・終了日・開始時刻(時・分)・終了時刻のリスト作成
+        startdates = values['startDate'].split(',')
+        enddates = values['endDate'].split(',')
+        starthours = values['startHour'].split(',')
+        startminutes = values['startMinute'].split(',')
+        endhours = values['endHour'].split(',')
+        endminutes = values['endMinute'].split(',')
+        print(f'statdates are {startdates}')
+        print(f'enddates  are {enddates} ')
+        print(f'starthours  are {starthours} ')
+        print(f'startminutes  are {startminutes} ')
+        print(f'endhours  are {endhours} ')
+        print(f'endminutes are {endminutes} ')
+        if len(startdates) != len(enddates):
+            enddates = startdates
+        if len(startdates) == len(enddates) == len(starthours) == len(startminutes) == len(endhours) == len(endminutes):
+            for i in range(0, len(startdates)):
+                # todo valuesの内容検証を！！
+                calendarEvent['summary'] = values['summary']
+                calendarEvent['location'] = values['location']
+                calendarEvent['description'] = values['description']
+                calendarEvent['start']['dateTime'] = generate_datetime(
+                    values['startYear'], values['startMonth'], startdates[i], starthours[i], startminutes[i])
+                calendarEvent['end']['dateTime'] = generate_datetime(
+                    values['endYear'], values['endMonth'], enddates[i], endhours[i], endminutes[i]
+                )
+                registration(calendarEvent)
+        elif len(starthours) == 1 and len(startdates) >= 1:
+            for i in range(0, len(startdates)):
+                calendarEvent['summary'] = values['summary']
+                calendarEvent['location'] = values['location']
+                calendarEvent['description'] = values['description']
+                calendarEvent['start']['dateTime'] = generate_datetime(
+                    values['startYear'], values['startMonth'], startdates[i], values['startHour'], values['startMinute'])
+                calendarEvent['end']['dateTime'] = generate_datetime(
+                    values['endYear'], values['endMonth'], enddates[i], values['endHour'], values['endMinute']
+                )
+                registration(calendarEvent)
+        elif len(startdates) == 1 and len(starthours) >= 1:
+            for i in range(0, len(starthours)):
+                calendarEvent['summary'] = values['summary']
+                calendarEvent['location'] = values['location']
+                calendarEvent['description'] = values['description']
+                calendarEvent['start']['dateTime'] = generate_datetime(
+                    values['startYear'], values['startMonth'], values['startDate'], starthours[i], startminutes[i])
+                calendarEvent['end']['dateTime'] = generate_datetime(
+                    values['endYear'], values['endMonth'], values['endDate'], endhours[i], endminutes[i]
+                )
+                registration(calendarEvent)
+        else:
+            return False
         # todo 終日イベントでないときも検証を行うように追加を
-        # insert calendar event
-        calendarEvent['summary'] = values['summary']
-        calendarEvent['location'] = values['location']
-        calendarEvent['description'] = values['description']
-        calendarEvent['start']['dateTime'] = generate_datetime(
-            values['startYear'], values['startMonth'], values['startDate'], values['startHour'], values['startMinute'])
-        calendarEvent['end']['dateTime'] = generate_datetime(
-            values['endYear'], values['endMonth'], values['endDate'], values['endHour'], values['endMinute']
-        )
-        registration(calendarEvent)
+        # # insert calendar event
+        # calendarEvent['summary'] = values['summary']
+        # calendarEvent['location'] = values['location']
+        # calendarEvent['description'] = values['description']
+        # calendarEvent['start']['dateTime'] = generate_datetime(
+        #     values['startYear'], values['startMonth'], values['startDate'], values['startHour'], values['startMinute'])
+        # calendarEvent['end']['dateTime'] = generate_datetime(
+        #     values['endYear'], values['endMonth'], values['endDate'], values['endHour'], values['endMinute']
+        # )
+        # registration(calendarEvent)
 
 
 def registration(calendarEvent):
@@ -140,6 +193,7 @@ def registration(calendarEvent):
     calendarEvent = service.events().insert(
         calendarId=config['CALENDAR']['calendarID'], body=calendarEvent).execute()
     print('予定追加完了')
+
 
 if __name__ == '__main__':
     add_schedules(values)
